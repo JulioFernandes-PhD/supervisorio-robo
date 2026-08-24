4c1e0e56dfc84a61556b9b7ecc2a756d37ce47c3
JulioFernandes-PhD <eng.juliofernandes@hotmail.com>
2026-08-24 17:56:37 -0300

    Add files via upload

--- leitor_clp_28.py
+++ leitor_clp_28.py
@@ -182,34 +182,41 @@ def worker_leitura_clp():
                     # PASSO 3 (13s até 23s): Ligar Y3 (Ativa vácuo/garra para pegar a peça)
                     elif 13 <= tempo_ciclo < 23:
                         ESTADO_SAIDAS["Y3"] = True
+                        ESTADO_ENTRADAS["X25"] = True  # Vacuostato confirma que pegou a peça
 
                     # PASSO 4 (23s até 23s - transição): Sobe Z (Y2 desliga automaticamente)
-
+                                            
                     # PASSO 5 (23s até 33s): Ligar Y0, Y1, Y4 (Vai para posição D: avança Y mantendo vácuo Y3)
                     elif 23 <= tempo_ciclo < 33:
                         ESTADO_SAIDAS["Y0"] = True
                         ESTADO_SAIDAS["Y1"] = True
                         #ESTADO_SAIDAS["Y4"] = True
                         ESTADO_SAIDAS["Y3"] = True  # Mantém peça presa durante o transporte
-
+                        ESTADO_ENTRADAS["X23"] = True
+                        
     # PASSO 6 (33s até 36s): Ligar Y2 (Desce eixo Z na Posição D)
                     elif 36 <= tempo_ciclo < 39:
                         ESTADO_SAIDAS["Y1"] = True  # Mantém Y posicionado
                         ESTADO_SAIDAS["Y2"] = True  # Desce Z
                         ESTADO_SAIDAS["Y3"] = True  # Mantém peça presa
-
+                        ESTADO_ENTRADAS["X23"] = True
+                        ESTADO_ENTRADAS["X24"] = True
+    
     # PASSO 7 (39s até 42s): Desliga Y3 (Solta a peça na posição D)
                     elif 39 <= tempo_ciclo < 42:
                         ESTADO_SAIDAS["Y1"] = True
                         ESTADO_SAIDAS["Y2"] = True
+                        ESTADO_ENTRADAS["X23"] = True
+                        ESTADO_ENTRADAS["X24"] = True
                         # Y3 fica False
 
     # PASSO 8 (42s até 45s): Desliga Y2 (Sobe Z sem a peça)
                     elif 42 <= tempo_ciclo < 45:
                         ESTADO_SAIDAS["Y1"] = True
+                        ESTADO_ENTRADAS["X23"] = True
                         # Y2 e Y3 ficam False
 
-        # PASSO 9 (45s até 49s): Desliga Y1 (Retorna Y para a origem A)
+    # PASSO 9 (45s até 49s): Desliga Y1 (Retorna Y para a origem A)
                     elif 45 <= tempo_ciclo < 49:
                         # Todas as saídas desligadas (Y1 desativa para retornar)
                         pass
